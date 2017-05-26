88a14c4a6997b03fae55d86f89de15b4cce66bee
merlin2v <nathan2visawsome@gmail.com>
2017-05-26 11:13:08 -0700

    Add files via upload

---
+++ src/projectfun/sprite/SpriteController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectfun.sprite;
+
+import javafx.scene.Scene;
+
+/** Controls a AbstractSprite and its movement
+X
+ * @author Nathan
+ */
+public abstract class SpriteController {
+    public abstract Scene getScene();
+    public abstract AbstractSprite getSprite();
+    
+}
