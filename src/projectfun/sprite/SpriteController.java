a79047d14f7f8b25ca1c2feefdee26dc40a2c578
merlin2v <nathan2visawsome@gmail.com>
2017-05-30 09:17:37 -0700

    Update SpriteController.java

--- src/projectfun/sprite/SpriteController.java
+++ src/projectfun/sprite/SpriteController.java
@@ -16,7 +16,7 @@ public abstract class SpriteController {
     public abstract AbstractSprite getSprite();
     public void move(double x,double y,double z){
         this.getSprite().setTranslateX(x);
-        this.getSprite().setTranslateX(y);
-        this.getSprite().setTranslateX(z);
+        this.getSprite().setTranslateY(y);
+        this.getSprite().setTranslateZ(z);
     }
 }
